8e71f76b2988470fc5772c1abebf2831ca6cd844
cyphergh <info.kusi.gh@gmail.com>
2024-09-23 12:12:12 +0000

    as

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en"  suppressHydrationWarning>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <body className={inter.className}>
       <ThemeProvider
             attribute="class"
